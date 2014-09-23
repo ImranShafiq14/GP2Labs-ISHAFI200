a52827405d80c00c5f15785775a41cd14c27adfd
ImranShafiq14 <imyshafiq@hotmail.co.uk>
2014-09-23 10:51:59 +0100

    Done up to page 10 step 8.1

--- GP2Labs-ISHAFI200/GP2Labs-ISHAFI200/main.cpp
+++ GP2Labs-ISHAFI200/GP2Labs-ISHAFI200/main.cpp
@@ -4,6 +4,9 @@
 //header for SDL2 functionality
 #include <SDL.h>
 
+#include <SDL_opengl.h>
+#include <gl\GLU.h>
+
 //Global variables go here 
 //Pointer to our SDL Windows
 SDL_Window * window;
@@ -11,6 +14,7 @@ SDL_Window * window;
 //Constants to control window creation
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
+bool running = true;
 
 
 //Global functions
@@ -26,15 +30,40 @@ void InitWindow(int width, int height, bool fullscreen)
 		);
 }
 
+//Used to cleanup once we exit
 void CleanUp()
 {
-
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 
 //Main Method - Entry Point
 int main(int argc, char * arg[])
 {
+	// init everything - SDL, if it is nonzero we have a problem
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "ERROR SDL_Init" << SDL_GetError() << std::endl;
+		return -1;
+	}
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
+	
+	SDL_Event event;
+	while (running)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			//Get event type
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
+			{
+				//set our boolean which controls the game loop to false
+				running = false;
+			}
+		}
+	}
+	
+	CleanUp();
 	return 0;
 }
 
