d099098baaaafcc36cf003ebb069e24198152e1d
ImranShafiq14 <imyshafiq@hotmail.co.uk>
2014-09-23 10:31:11 +0100

    Done up to page 6 step 5.4

---
+++ GP2Labs-ISHAFI200/GP2Labs-ISHAFI200/main.cpp
@@ -0,0 +1,43 @@
+//Header files
+#include <iostream>
+
+//header for SDL2 functionality
+#include <SDL.h>
+
+//Global variables go here 
+//Pointer to our SDL Windows
+SDL_Window * window;
+
+//Constants to control window creation
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+
+
+//Global functions
+void InitWindow(int width, int height, bool fullscreen)
+{
+	//Create a window 
+	window = SDL_CreateWindow(
+		"Lab 1",		//window title
+		SDL_WINDOWPOS_CENTERED,		//x position, centered
+		SDL_WINDOWPOS_CENTERED,		//y position, centered
+		width,				//width, in pixels
+		height				//height, in pixels
+		SDL_WINDOW_OPENGL		//flags
+		);
+}
+
+void CleanUp()
+{
+
+}
+
+
+//Main Method - Entry Point
+int main(int argc, char * arg[])
+{
+	return 0;
+}
+
+
+
