c7db66be6239e56c8bd0764bc26c90450e42f6c9
ImranShafiq14 <imyshafiq@hotmail.co.uk>
2014-10-28 15:45:23 +0000

    Lab 6 - Done up to step 2.2 page 5 -ERRORS

---
+++ GP2Labs-ISHAFI200/GP2Labs-ISHAFI200/GameObject.h
@@ -0,0 +1,36 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Component;
+
+class GameObject
+{
+public:
+	GameObject();
+	virtual ~GameObject();
+	
+	void init();
+	void update();
+	void render();
+	void destroy();
+	
+	const std::string& getName();
+	void setName(std::string& name);
+	void addComponent(Component * component);
+protected:
+	//std::string m_Type;
+	//bool m_Active;
+private:
+	std::string m_Name;
+	vector<Component*> m_Components;
+};
+
+
+
+
+#endif
