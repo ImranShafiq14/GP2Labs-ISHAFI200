1e6b07e5bc023bc14dccb1b1ba074715a6de3a4b
Imran Shafiq <imyshafiq@hotmail.co.uk>
2014-10-29 12:17:16 +0000

    Lab 6 - Done up to step 4 page 8

---
+++ GP2Labs-ISHAFI200/GP2Labs-ISHAFI200/Mesh.cpp
@@ -0,0 +1,69 @@
+#include "Mesh.h"
+#include "Vertex.h"
+
+Mesh::Mesh()
+{
+
+}
+
+Mesh::~Mesh()
+{
+
+}
+
+void Mesh::init()
+{
+	glGenVertexArrays(1, &m_VAO);
+	glBindVertexArray(m_VAO);
+	//Create buffer
+	glGenBuffers(1, &m_VBO);
+	//Make the new VBO active
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+	//Create buffer
+	glGenBuffers(1, &m_EBO);
+	//Make the new EBO active
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)sizeof(vec3));
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)(sizeof(vec3) + sizeof(vec2)));
+}
+
+void Mesh::destroy()
+{
+	glDeleteBuffers(1, &m_EBO);
+	glDeleteBuffers(1, &m_VBO);
+	glDeleteVertexArrays(1, &m_VAO);
+}
+
+void Mesh::Bind()
+{
+	glBindVertexArray(m_VAO);
+}
+
+void Mesh::copyVertexData(int count, int stride, void** data)
+{
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+	glBufferData(GL_ARRAY_BUFFER, count*stride, data, GL_STATIC_DRAW);
+	m_VertexCount = count;
+}
+
+void Mesh::copyIndexData(int count, int stride, void** data)
+{
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, count*stride, data, GL_STATIC_DRAW);
+	m_IndexCount = count;
+}
+
+int Mesh::getIndexCount()
+{
+	return m_IndexCount;
+}
+
+int Mesh::getVertexCount()
+{
+	return m_VertexCount;
+}
---
+++ GP2Labs-ISHAFI200/GP2Labs-ISHAFI200/Mesh.h
@@ -0,0 +1,28 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "Component.h"
+#include <gl\glew.h>
+
+class Mesh : public Component
+{
+public:
+	Mesh();
+	~Mesh();
+
+	void init();
+	void destroy();
+	void copyVertexData(int count, int stride, void** data);
+	void copyIndexData(int count, int stride, void** data);
+	void Bind();
+	int getVertexCount();
+	int getIndexCount();
+private:
+	GLuint m_VBO;
+	GLuint m_EBO;
+	GLuint m_VAO;
+	int m_VertexCount;
+	int m_IndexCount;
+};
+
+#endif
